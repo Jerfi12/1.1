1c43fc502bdcb2a8539c9a54daaf1c8020ee9703
Jerfi12 <dima.dima.stepanov79@mail.ru>
2025-02-28 09:55:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Задание_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            while(userInput != "exit")
+            {
+                Console.Write("Введите что-нибудь (или 'exit' для выхода):");
+                userInput = Console.ReadLine();
+                
+                if(userInput != "exit")
+                {
+                    Console.WriteLine($"Вы ввели: {userInput}");
+                }
+            }
+            Console.WriteLine("Вы завершили программу");
+        }
+    }
+}
